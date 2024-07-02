2b70a6a71964649b98ecae3e61f4bff9e48aac0f
Ibrahim Rashwan <ibrahimrashwan63@gmail.com>
2024-07-02 21:40:48 +0300

    add chart in summary, and add search for posts

--- src/app/modules/dashboard/components/summary/summary.component.ts
+++ src/app/modules/dashboard/components/summary/summary.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { SUMMARY_FOOTER_STATISTICS, SUMMARY_HEADER_STATISTICS } from '@modules/dashboard/constants/statistics.constant';
 import { IStatisticsItem } from '@modules/dashboard/models/statistics.model';
 import { StatisticsCardComponent } from '../statistics-card/statistics-card.component';
+import { ChartConfiguration } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
   imports: [
-    StatisticsCardComponent
+    StatisticsCardComponent,
+    BaseChartDirective
   ],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
@@ -15,4 +18,41 @@ import { StatisticsCardComponent } from '../statistics-card/statistics-card.comp
 export class SummaryComponent {
   headerStatistics: IStatisticsItem[] = SUMMARY_HEADER_STATISTICS;
   footerStatistics: IStatisticsItem[] = SUMMARY_FOOTER_STATISTICS;
+  barChartLegend = true;
+  barChartPlugins = [];
+  dataCount:number = 7;
+  numberCFG = {count: this.dataCount, min: 0, max: 100};
+
+  barChartData: ChartConfiguration<'line' | 'bar'>['data'] = {
+    labels: [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ],
+    datasets: [
+      {
+        label: '',
+        data: [ 65, 59, 50, 81, 56, 55, 40 ],
+        borderColor: "#828ca0",
+        backgroundColor: "#828ca0",
+        stack: 'combined',
+        type: 'bar'
+      },
+      {
+        label: '',
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        borderColor: "#828ca0",
+        backgroundColor: "#828ca0",
+        stack: 'combined',
+        barThickness: 20,
+        pointBorderWidth: 10,
+        pointStyle: 'rect'
+      }
+    ]
+  };
+
+  barChartOptions: ChartConfiguration<'bar' | 'line'>['options'] = {
+    responsive: false,
+    scales: {
+      y: {
+        stacked: true
+      }
+    }
+  };
 }
--- src/app/modules/post/components/post.component.ts
+++ src/app/modules/post/components/post.component.ts
@@ -1,25 +1,72 @@
-import { Component, Signal, inject } from '@angular/core';
+import { Component, Signal, inject, OnInit, signal, WritableSignal, DestroyRef} from '@angular/core';
 import { PostService } from '../services/post.service';
 import { IPost } from '../models/post.model';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { PostCardComponent } from './post-card/post-card.component';
 import { RouterLink } from '@angular/router';
 import { finalize } from 'rxjs/internal/operators/finalize';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { map } from 'rxjs/internal/operators/map';
+import { debounceTime } from 'rxjs/internal/operators/debounceTime';
 
 @Component({
   selector: 'app-post',
   standalone: true,
   imports: [
     PostCardComponent,
-    RouterLink
+    RouterLink,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
-export default class PostComponent {
+export default class PostComponent implements OnInit {
   #postService: PostService = inject(PostService);
+  #destroyRef: DestroyRef = inject(DestroyRef);
   isLoading: boolean = true;
+  keywordFormControl = new FormControl();
+  filteredPosts: WritableSignal<IPost[] | undefined> = signal([]);
   posts: Signal<IPost[] | undefined> = toSignal(
     this.#postService.getList().pipe(finalize(() => this.isLoading = false))
+    .pipe(map(posts => {
+      this.filteredPosts.set(posts);
+      return posts
+    }))
   );
+
+  ngOnInit():void {
+    this.onKeywordFormControlChange();
+  }
+
+  onKeywordFormControlChange():void {
+    this.keywordFormControl.valueChanges
+    .pipe(debounceTime(500), takeUntilDestroyed(this.#destroyRef))
+    .subscribe( keyword => this.filteredPosts.set(this.getFiltredPosts(keyword)))
+  }
+
+  getFiltredPosts(keyword:string):IPost[] {
+    if(keyword?.length){
+      const KEYWORD_FORMATTED = keyword?.toLocaleLowerCase();
+      return this.posts()?.filter(post => {
+        // i make extra vars to make my code readable
+        const BODY = post.body?.toLowerCase();
+        const TITLE = post.body?.toLowerCase();
+        return BODY?.includes(KEYWORD_FORMATTED) || TITLE?.includes(KEYWORD_FORMATTED);
+      })!
+    } else {
+      return this.posts()!
+    }
+  }
+
 }
